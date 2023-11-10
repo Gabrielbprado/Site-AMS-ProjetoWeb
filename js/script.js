c05b5e736125cb344d9018e5d1a1208a5e266b46
AlessandroJUNIOR1320211 <alessandroiranojr@gmsil.com>
2023-11-10 10:46:09 -0300

    Site Ready

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-var swiper = new Swiper('.swiper-container', {
-    // Opções do carrossel
-  });
-  
+let posicaoAtual = 0;
+
+function moverCarrossel(direcao) {
+    const carrossel = document.getElementById('carrossel');
+    const larguraImagem = document.querySelector('.img-carrossel').clientWidth;
+
+    posicaoAtual += direcao;
+
+    if (posicaoAtual > (carrossel.children.length - 1)) {
+        posicaoAtual = 0;
+    }
+
+    if (posicaoAtual < 0) {
+        posicaoAtual = carrossel.children.length - 1;
+    }
+
+    carrossel.style.transform = `translateX(${-posicaoAtual * larguraImagem}px)`;
+}
